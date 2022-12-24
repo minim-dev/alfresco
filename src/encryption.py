4324595ab0663cb7187ea0d13d859245c11b3867
Ahmad Shahzad <minim-dev@outlook.com>
2022-12-24 18:21:48 +0500

    Initial commit.

---
+++ src/encryption.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from passlib.hash import nthash
+
+input = open('/tmp/password','r')
+a = input.read().replace("\n", "").replace("\r", "")
+print(a)
+h = nthash.hash(a)
+print(h)
+
+output = open('/tmp/password', 'w')
+output.write(h)
+output.close()
