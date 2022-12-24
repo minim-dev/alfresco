d4a0685f6c5862565c649970a48a038bdf2d1057
Ahmad Shahzad <minim-dev@outlook.com>
2022-12-24 18:28:57 +0500

    Fifth commit.

--- src/encrypt.sh
+++ src/encrypt.sh
@@ -6,5 +6,6 @@ pip3 install passlib >/dev/null 2>&1
 echo "$1" > /tmp/password
 cd /root/alfresco/src
 python3 encryption.py > /dev/null
-echo -e "Password: ${cat /tmp/password}"
+echo -e "Password: $1"
+echo -e "Encrypted Password: $(cat /tmp/password)"
 echo -e "\n"
